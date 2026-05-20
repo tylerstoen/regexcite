7ef84d4b709da23b881f5e44ddbef89208ad8bc4
tylerstoen <tyler.stoen@gmail.com>
2026-05-19 20:51:18 -0700

    add functions and warnings

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -21,3 +21,56 @@ str_split_one <- function(string, pattern, n = Inf) {
     character()
   }
 }
+
+#' Split a string with commas
+#' @param string A character vector with, at most, one element.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_comma(x, pattern = ",")
+#' str_split_comma(x, pattern = ",", n = 2)
+#'
+#' y <- "192.168.0.1"
+#' str_split_comma(y)
+str_split_comma <- function(string, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    str_split_one(string = string, pattern = ",", n=n)
+  } else{
+    character()
+  }
+}
+
+
+#' Split a string of numbers
+#' @param string A character vector of numbers with, at most, one element.
+#'
+#' @return A numeric vector.
+#' @export
+#'
+#' @examples
+#' #' y <- "192.168.0.1"
+#' str_split_nums(y)
+#'
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_nums(x, pattern = ",")
+#' str_split_nums(x, pattern = ",", n = 2)
+#'
+str_split_nums <- function(string, pattern, n=Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    parts <- str_split_one(string = string, pattern = pattern, n=n)
+    nums <- suppressWarnings(as.numeric(parts))
+
+    if (any(is.na(nums))) {
+      warning("String includes non-numeric characters", call. = FALSE)
+    }
+
+    nums
+  } else {
+    numeric()
+  }
+}
